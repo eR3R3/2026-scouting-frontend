25f9db8c2cc1a7f15306f6e4638a919fb2471b96
juncheng fu <fujuncheng25@shphschool.com>
2026-03-16 12:51:46 +0800

    Change iframe source URL in dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,8 @@
 const Page = () => {
   return (
     <iframe
-      src="https://scout.tail768c11.ts.net/"
+      //src="https://scout.tail768c11.ts.net/"
+      src="https://debian.taila41dd6.ts.net/"
       style={{ width: "100%", height: "100vh", border: "none" }}
     />
   )
